1fa1167035b8ad670fb1acf5f12ba62c32abd046
Suareguen <adrian.suarez.monzon@gmail.com>
2023-05-04 10:27:50 +0100

    General: - Member can delete profile

--- an-frontend/src/pages/profile/delete/delete.jsx
+++ an-frontend/src/pages/profile/delete/delete.jsx
@@ -1,25 +1,28 @@
 import React from 'react'
 import deleteMember from '../../../services/deleteMember'
-import { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, redirect } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+
 
 
 
 const Delete = () => {
-    // useEffect(() => {
-    //     deleteMemberById()
-    // }, [])
     const { id } = useParams()
+    const navigate = useNavigate()
     console.log(id)
 
     const deleteMemberById = async () => {
         await deleteMember(id)
         localStorage.removeItem('token')
-        alert('member delete')
+        alert('Account deleted')
+        return navigate('/login')
     }
 
   return (
-    <button onClick={deleteMemberById}>Delete Member</button>
+    <>
+      <h2>Are you Sure?</h2>
+      <button onClick={deleteMemberById}>Delete Account</button>
+    </>
   )
 }
 
