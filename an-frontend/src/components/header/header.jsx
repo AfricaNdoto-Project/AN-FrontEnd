02542b45c2d568ca4ab7f4674185e0acac54c43e
Adrián <103899316+Suareguen@users.noreply.github.com>
2023-05-12 13:43:44 +0100

    Delete an-frontend directory
    delete folder

--- an-frontend/src/components/header/header.jsx
+++
@@ -1,9 +0,0 @@
-import './header.css'
-import ButtonAppBar from '../appBar/appBar'
-const Header = () => {
-  return (
-    <ButtonAppBar />
-  )
-}
-
-export default Header
--- an-frontend/src/pages/home/home.jsx
+++
@@ -1,8 +0,0 @@
-import './home.css'
-const Home = () => {
-  return (
-    <div className='container'>Body</div>
-  )
-}
-
-export default Home
